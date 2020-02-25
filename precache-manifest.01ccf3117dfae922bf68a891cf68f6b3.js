08b2687dba88299da319b17492973df2bc81f268
igor29 <igmatviytsev@gmail.com>
2020-02-25 16:44:00 +0200

    Updates

---
+++ precache-manifest.01ccf3117dfae922bf68a891cf68f6b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fbe7c106302f9f6f7eaee3c68c10af",
+    "url": "/pill.plate/index.html"
+  },
+  {
+    "revision": "e0730f22d1d74e4b491c",
+    "url": "/pill.plate/static/css/2.59f4f2a6.chunk.css"
+  },
+  {
+    "revision": "f9085b9fcf2d44a97e40",
+    "url": "/pill.plate/static/css/main.d47b9a7d.chunk.css"
+  },
+  {
+    "revision": "e0730f22d1d74e4b491c",
+    "url": "/pill.plate/static/js/2.ae6c3651.chunk.js"
+  },
+  {
+    "revision": "118508571645c9afc420188f9f4bff99",
+    "url": "/pill.plate/static/js/2.ae6c3651.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9085b9fcf2d44a97e40",
+    "url": "/pill.plate/static/js/main.3ece52a6.chunk.js"
+  },
+  {
+    "revision": "ab85c7500149f28b7f2f",
+    "url": "/pill.plate/static/js/runtime-main.17289622.js"
+  },
+  {
+    "revision": "9f6804c4c52fcd3b1e60a12a701f97f6",
+    "url": "/pill.plate/static/media/idea.9f6804c4.png"
+  },
+  {
+    "revision": "4608d02c877fc22ad189780e6b7aaf32",
+    "url": "/pill.plate/static/media/soon.4608d02c.png"
+  }
+]);
